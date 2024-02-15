3e23ba51cc333448eec77e27c5110dd0af815ba2
Ashley Rarieya <ararieya@gmail.com>
2024-02-15 21:51:13 +0300

    routes now working

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from resources.carbon import CarbonFootprintCalculation
 from config import app, api
 from resources.edu_resources import EduResource
 from resources.user import UserAccounts, SignUp, Login
-from resources.events import Events
-from resources.partners import Partners
+from resources.events import EventsResource
+from resources.partners import PartnerResource
 
 api= Api(app)
 
@@ -17,8 +17,8 @@ api.add_resource(SignUp, '/signup')
 api.add_resource(Login, '/login')
 api.add_resource(ImpactMonitorings, '/impact_monitorings')
 api.add_resource(CarbonFootprintCalculation, '/calculate_footprint')
-# api.add_resource(Events, '/events')
-# api.add_resource(Partners, '/partners')
+api.add_resource(EventsResource, '/events')
+api.add_resource(PartnerResource, '/partners')
 
 
 if __name__ == '__main__':
