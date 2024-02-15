0be730b641942e2c4d8cb3eb613d35330dfb2dd5
IanGichane <gichaneian@gmail.com>
2024-02-15 20:25:22 +0300

    app.pay add routes

--- app.py
+++ app.py
@@ -1,18 +1,12 @@
 from config import app
 from flask_restful import Api
 
-from resources.feedback import Feedback
-from resources.userEvent import User_Event
+
 
 api = Api(app)
 
 
-api.add_resource(Feedback, '/feedback', "/feedback/<int:id>")
-api.add_resource(User_Event, '/event_user')
 
-
-if __name__ == '__main__':
-    app.run(port=5555, debug=True)
 from flask import Flask
 from flask_restful import Api
 from resources.impact import ImpactMonitorings
@@ -22,6 +16,8 @@ from resources.edu_resources import EduResource
 from resources.user import UserAccounts
 from resources.events import Events
 from resources.partners import Partners
+from resources.feedback import Feedback
+from resources.userEvent import User_Event
 
 # Add resources to the API
 api.add_resource(EduResource, '/education-resources')
@@ -30,6 +26,8 @@ api.add_resource(ImpactMonitorings, '/impact_monitorings')
 api.add_resource(CarbonFootprintCalculation, '/calculate_footprint')
 api.add_resource(Events, '/events')
 api.add_resource(Partners, '/partners')
+api.add_resource(Feedback, '/feedback', "/feedback/<int:id>")
+api.add_resource(User_Event, '/event_user')
 
 from flask_restful import Api
 
