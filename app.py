327d0d2b2edb20d5d6054c1166908da4bfa3b3c6
IanGichane <gichaneian@gmail.com>
2024-02-15 16:34:54 +0300

    user events and feedback routes

--- app.py
+++ app.py
@@ -16,6 +16,17 @@ api.add_resource(CarbonFootprintCalculation, '/calculate_footprint')
 api.add_resource(Events, '/events')
 api.add_resource(Partners, '/partners')
 
+from flask_restful import Api
+
+from resources.feedback import Feedback
+from resources.userEvent import User_Event
+
+api = Api(app)
+
+
+api.add_resource(Feedback, '/feedback', "/feedback/<int:id>")
+api.add_resource(User_Event, '/event_user')
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
