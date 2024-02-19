7f1b459f87238443e468437cb0f3e405082db684
Johnson Mbeba <john.mbeba@student.moringaschool.com>
2024-02-19 21:37:09 +0300

    Update api resources

--- resources/events.py
+++ resources/events.py
@@ -19,11 +19,12 @@ class EventsResource(Resource):
         
     parser.add_argument('title', type=str, required=True, help='Title is required')
     parser.add_argument('description', type=str, required=True, help='Description is required')
-    parser.add_argument('location', type=str, required=True, help='Category is required')
+    parser.add_argument('location', type=str, required=True, help='Location is required')
+    parser.add_argument('image_url', type=str, required=True, help='Image url is required')
     parser.add_argument('date_event', required=True, help='Content is required')
     parser.add_argument('organizer', type=str, required=True, help='Author is required')
-    parser.add_argument('contact_info', type=str, required=True, help='Author is required')
-    parser.add_argument('registration_deadline', required=True, help='Date published is required')
+    parser.add_argument('contact_info', type=str, required=True, help='Contact info is required')
+    parser.add_argument('registration_deadline', required=True, help='Registration deadline is required')
         
     def get(self):
         events = Events.query.all()
