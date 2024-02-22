c80829428734d808008ab3f0fea5ab611d35d918
caroline opiyo <opiyocaroline20@gmail.com>
2024-02-23 00:49:31 +0300

    Handle validation errors in request data parsing

--- resources/user.py
+++ resources/user.py
@@ -2,13 +2,20 @@ from flask_restful import Resource, reqparse
 from models import Users
 from config import db, bcrypt
 
-class UserAccounts(Resource):    
+# Define UserAccounts class to handle user accounts
+class UserAccounts(Resource): 
+    
+    # GET method to fetch all users   
     def get(self):
+        # Retrieve all users from the database and convert them to dictionary format
         users = [user.to_dict() for user in Users.query.all()]
         
+         # Return the list of users along with a success status code
         return users,200
     
+    # POST method to create a new user
     def post(self):
+        # Create a request parser to parse incoming data
         parser = reqparse.RequestParser()
         
         parser.add_argument('first_name', type=str, required=True, help='First name is required')
@@ -22,14 +29,20 @@ class UserAccounts(Resource):
         parser.add_argument('interests', required=True, help='Interests is required')
         parser.add_argument('age', required=True, help='Age is required')
         
+        # Parse the incoming data
         args = parser.parse_args()
         
+        # Hash the password using bcrypt before storing in the database
         args['password'] = bcrypt.generate_password_hash(args['password'].encode('utf-8'))
         
+        # Create a new user object with parsed data
         new_user = Users(**args)
+        
+        # Add the new user to the database session and commit the transaction
         db.session.add(new_user)
         db.session.commit()
         
+        # Return success message along with a success status code
         return {
             "message":"User created successfully"
         },201
