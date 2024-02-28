54156efbc57eca31f89daf8d3012662cc11f81e8
Johnson Mbeba <john.mbeba@student.moringaschool.com>
2024-02-29 02:26:06 +0300

    Add location statistics to summary endpoint

--- resources/summary.py
+++ resources/summary.py
@@ -9,13 +9,21 @@ class Summary(Resource):
         num_of_bookings = UserEvents.query.count()
         
         categories = set([category.category for category in Products.query.with_entities(Products.category).all()])
+        locations = set([location.location for location in Events.query.with_entities(Events.location).all()])
     
         category_stats = []
+        location_stats = []
         
         for category in categories:
             category_stats.append({
                 "category":category,
                 "count":Products.query.filter(Products.category == category).count()
+            })
+            
+        for location in locations:
+            location_stats.append({
+                "location":location,
+                "count":Events.query.filter(Events.location == location).count()
             })
         
         return {
@@ -23,5 +31,6 @@ class Summary(Resource):
             "products":num_of_products,
             "events":num_of_events,
             "bookings":num_of_bookings,
-            "category_stats":category_stats
+            "category_stats":category_stats,
+            "location_stats":location_stats
         }, 200
